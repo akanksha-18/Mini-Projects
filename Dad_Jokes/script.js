26e7a5ed33c1fc48bfe9da1ce41e7f83e842f4c0
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-03-12 23:06:49 +0530

    commit

--- Dad_Jokes/script.js
+++ Dad_Jokes/script.js
@@ -5,32 +5,36 @@ const spinnerEl = document.getElementById("spinner");
 const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
 
 const options = {
-  method: "GET",
-  headers: {
-    "X-Api-Key": apiKey,
-  },
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
 };
 
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke() {
-  try {
-    jokeEl.innerText = "Updating...";
-    btnEl.disabled = true;
-    btnEl.innerText = "Loading...";
-    const response = await fetch(apiURL, options);
-    const data = await response.json();
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+        spinnerEl.style.display = "inline-block"; // Show spinner
 
-    btnEl.disabled = false;
-    btnEl.innerText = "Tell me a joke";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
 
-    jokeEl.innerText = data[0].joke;
-  } catch (error) {
-    jokeEl.innerText = "An error happened, try again later";
-    btnEl.disabled = false;
-    btnEl.innerText = "Tell me a joke";
-    console.log(error);
-  }
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+        spinnerEl.style.display = "none"; // Hide spinner
+
+        jokeEl.innerText = data[0].joke;
+    } catch (error) {
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+        console.log(error);
+        spinnerEl.style.display = "none"; // Hide spinner in case of an error
+    }
 }
 
 btnEl.addEventListener("click", getJoke);
